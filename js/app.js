9ef512937918164e06c682e384c86ca1dcf7ebf6
davidharnisch <davidharnisch@seznam.cz>
2021-12-02 21:47:08 +0100

    Final

--- js/app.js
+++ js/app.js
@@ -1,19 +1,120 @@
 //Use an IIFE to avoid contanminating global namespce
 (function(){
- 
-  
+
+
+ //Grab stores items from the DOM
+ let storeItems = document.querySelectorAll('.store-item');
+ //grab lightbox container 
+ let lightBox = document.querySelector('.lightbox-container');
+  //grab the div with the lightbox item
+ let lightBoxItem = document.querySelector('.lightbox-item');
+ //grab all the images from the store items
+ let images = document.querySelectorAll('.store-img');
+
+
+ let imageList = [];
+ let imageCounter = 0;
+
+ images.forEach(function(image){
+  //push each imageto the array of images
+  imageList.push(image.src);
+})
+
+
+
+
+storeItems.forEach(function(item) {
+
+  item.addEventListener('click', function(e){
+
+    let image = e.target.src;
+    //change the background img property of the lightbox item
+    lightBoxItem.style.backgroundImage = `url(${image})`;
+
+    //lightBoxItem.style.backgroundImage = "url("+ image + ")";
+
+    //show the modal with the selected image
+    lightBox.classList.add('show');
+    //get the array index number for the image that was selected
+    imageCounter = imageList.indexOf(image);
+
+    console.log(imageCounter);
+    
+    
+  });
+
+});
+
+
+let buttonLeft = document.querySelector('.btnLeft');
+let buttonRight = document.querySelector('.btnRight');
+
+buttonLeft.addEventListener("click", changeImageLeft);
+buttonRight.addEventListener("click", changeImageRight);
+
+function changeImageLeft() {
+        
+  imageCounter--;
+  if (imageCounter<0) {
+    imageCounter = imageList.length - 1
+  }
+  lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`
+}
+
+function changeImageRight() {
+        
+  imageCounter++;
+  if (imageCounter> imageList.length -1) {
+    imageCounter = 0
+  }
+  lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`
+}
+
+
+
 
   
+
+let lightBoxClose = document.querySelector('.lightbox-close');
+    lightBoxClose.addEventListener('click', function(){
+    lightBox.classList.remove('show');
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  // const storeImage = document.querySelectorAll('.store-img') 
+  
+  // storeImage.forEach(function(image){
+  //   image.addEventListener('click', function(e){
+
+  //     const ImgPath = e.target.getAttribute('src');
+  //     console.log(ImgPath)
+  //     const element = document.getElementsByClassName("lightbox-container");
+  //     //const element = document.getElementById("lightbox-container");
+
+  //     const lightboxItem = document.getElementsByClassName("lightbox-item");
+  //     element[0].classList.add("show");
+  //     //lightboxItem[0].style.background = "url('../img/cake-2.jpeg')"
+  //     lightboxItem[0].style.background = "url(../"+ ImgPath + ")";
+
+  //     console.log("url(../"+ ImgPath + ")");
+  
+  
+
+//     })
+// })
+
 })();
 
 
-/*
-Notes during project
-1. Tried to add an event listener to a node list. Had to actually do a forEach on the node list.
-https://stackoverflow.com/questions/55667793/uncaught-typeerror-input-addeventlistener-is-not-a-function
-2. Tried to use style.classList and then .show instead of just the string show. 'style not needed'
-3. How to access the individual CSS properties (without simply adding a class)
-4. I struggled with change the background. I thought I would be able to simply use element.style.background = 'image' but I realized I needed to use 'url(img);
-5. Had a bug in my code where I misspelled background. backgound vs. background 
-
-*/
